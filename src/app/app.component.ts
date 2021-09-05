9fdf750fcce7d91cda1d18d39e21b67dbf2e0a36
thao0406 <thaodhbk95@gmail.com>
2021-09-05 10:36:59 +0700

    Form Reactive Assignment ThaoNV12

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Form-Reactive-Assignment';
+export class AppComponent implements OnInit {
+  projectForm: FormGroup;
+  invalidProjectName(control: FormControl): {[s: string]: boolean} {
+    if (control.value === 'Test') {
+      return {'invalidProjectName': true};
+    }
+    return null;
+  }
+
+  asyncInvalidProjectName(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'Testproject') {
+          resolve({'invalidProjectName': true});
+        } else {
+          resolve(null);
+        }
+      }, 2000);
+    })
+    return promise;
+  }
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(
+        null,
+        [Validators.required, this.invalidProjectName],
+        this.asyncInvalidProjectName
+      ),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'projectStatus': new FormControl('critical')
+    });
+  }
+
+  onSaveProject() {
+    console.log(this.projectForm.value);
+  }
 }
